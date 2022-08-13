b58d86aa0794ff6e1c5a93ca4a4dfb791bb592e6
Dev-Amaro <joao.guilherme1827@gmail.com>
2022-08-13 09:06:28 -0300

    Crição das opções e suas telas

--- login.js
+++ login.js
@@ -2,24 +2,18 @@ var checkbox = document.getElementById('auditivo');
 var checkbox1 = document.getElementById('visuais');
 var checkbox2 = document.getElementById('naotem');
 function proximo(){
+    if(document.getElementById("nome").value == ""){
+        alert('Por favor, preencha o campo nome');
+        document.getElementById("nome").focus();
+        return false
+    }
     if(checkbox.checked){
         window.open("auditivos.html", "_self");
-    }
-    else{
-        console.log("erro");
-    }
-
-    if(checkbox1.checked){
+    }if(checkbox1.checked){
         window.open("visuais.html", "_self");
-    }
-    else{
-        console.log("erro1");
-    }
-
-    if(checkbox2.checked){
+    }if(checkbox2.checked){
         window.open("inicio.html", "_self");
-    }
-    else{
-        console.log("erro2");
+    }else{
+        alert("Selecione uma opção");
     }
 }
